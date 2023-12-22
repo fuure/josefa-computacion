dc1833bcc63d5a4ad96e4d881c772c4780ef1b6d
Alejandro Ruz <alejandro.ruz@consorcio.cl>
2023-12-22 09:09:06 -0300

    Mejoras generales

--- josefasite/run.sh
+++ josefasite/run.sh
@@ -1,4 +1,4 @@
 python3.9 manage.py makemigrations
-python3.9 manage.py migrate --fake useradmin zero
+#python3.9 manage.py migrate --fake useradmin zero
 python3.9 manage.py migrate useradmin
 python3.9 manage.py runserver
